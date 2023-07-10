1f85a04a970aee79a82fb3618f9a3aa94f5a906e
Rehaan Mohammed <mohammedrehaan.work@gmail.com>
2023-07-10 05:49:25 +0530

    Add the syntax crate

---
+++ crate/toad_writer_syntax/src/lib.rs
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright 2023, (Feohr) Mohammed Rehaan and the ToadWriter contributors.
+
+//! Library for handling the format of Toad Writer documents.
